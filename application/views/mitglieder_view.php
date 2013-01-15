5106f03e8dfaf963ede8c19be952630d687b2630
robertoceschi <robertoceschi@parobri.ch>
2013-01-15 16:45:09 +0100

    bugfix search-field version2

--- application/views/mitglieder_view.php
+++ application/views/mitglieder_view.php
@@ -124,8 +124,6 @@
         });
 
 
-
-
     });
 </script>
 
@@ -187,10 +185,9 @@
                         <thead>
                         <tr>
                             <!--<th width="20%">id</th> -->
-
                             <th width="20%">Vorname</th>
                             <th width="20%">Nachname</th>
-                            <th width="10%">Status</th>
+                            <!--<th width="10%">group</th>-->
                             <th width="2%">active</th>
                             <th width="2%">Delete</th>
                             <th width="2%">Edit</th>
