1a11685caf92ee99523d945af6ca3d27e593617c
robertoceschi <robertoceschi@parobri.ch>
2013-01-16 15:47:46 +0100

    bugfixes an ignited datatables

--- application/views/mitglieder_view.php
+++ application/views/mitglieder_view.php
@@ -119,7 +119,7 @@
         $('#example').dataTable({
             "bProcessing":true,
             "bServerSide":true,
-            "aoColumns" : [{"bSearchable": true}, {"bSearchable": true}, {"bSearchable": true}, {"bSearchable": false}, {"bSearchable": true}, {"bSearchable": true} ],
+            "aoColumns" : [{"bSearchable": true}, {"bSearchable": true}, {"bSearchable": false}, {"bSearchable": false}, {"bSearchable": false}, {"bSearchable": false} ],
             "sServerMethod":"POST"  ,
             "sAjaxSource":"<?php base_url();?>ajax/getdatabyajax"
         });
