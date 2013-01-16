ce308a692f12a733dbfdf5f76459404884895d2d
robertoceschi <robertoceschi@parobri.ch>
2013-01-16 14:59:48 +0100

    anpassungen an ignited datatables

--- application/views/mitglieder_view.php
+++ application/views/mitglieder_view.php
@@ -187,7 +187,7 @@
                             <!--<th width="20%">id</th> -->
                             <th width="20%">Vorname</th>
                             <th width="20%">Nachname</th>
-                            <!--<th width="10%">group</th>-->
+                            <th width="10%">group</th>
                             <th width="2%">active</th>
                             <th width="2%">Delete</th>
                             <th width="2%">Edit</th>
